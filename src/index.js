f3df9041dab2fe43d7f4a1730863a0ef85d5f262
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-10-26 18:54:28 +0100

    Add html markup, import and install fontawesome

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-unused-vars */
+import _ from 'lodash';
+import './style.css';
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
+
