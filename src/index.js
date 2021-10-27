f69757207dcbdac4cac73dc9e61c5f89f5f8dc57
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-10-27 18:19:55 +0100

    Add check.js file

--- src/index.js
+++ src/index.js
@@ -1,36 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import './style.css';
 
-const toDoList = [
-  {
-    description: 'Complete webpack excercise',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'Setup to do project',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Submit a code review',
-    completed: false,
-    index: 2,
-  },
-];
 
-function renderList() {
-  const list = document.getElementById('to_do_list');
-  list.innerHTML = '';
-  toDoList.forEach((e) => {
-    list.innerHTML += `<li class="list_container">
-    <div class="checkbox_container">
-      <input type="checkbox">
-      <p>${e.description}</p>
-    </div>
-    <i class="fas fa-ellipsis-v"></i>
-  </li>`;
-  });
-}
+let toDoList = [];
 
-window.addEventListener('load', renderList);
+
