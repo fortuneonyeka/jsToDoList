44fe694ba52a66e67b51ee006f7c5963b051fe23
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-10-27 18:21:54 +0100

    Add DOM variables

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@
 import './style.css';
 import './check';
 
+
+const input = document.querySelector(".to_do_input")
+const addBtn = document.querySelector(".submit_btn")
+const list = document.getElementById('to_do_list');
+
 let toDoList = [];
 
 
